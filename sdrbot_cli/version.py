e2dfc0292e73b5c3d9aac97a6196717a552fd0fe
Ara Kevonian <avonian@users.noreply.github.com>
2025-12-14 18:48:27 +0100

    chore: bump version to 0.1.0a3

--- sdrbot_cli/version.py
+++ sdrbot_cli/version.py
@@ -5,4 +5,4 @@ try:
 except PackageNotFoundError:
     # If the package is not installed (e.g. running from source without install),
     # fallback to the version defined in pyproject.toml
-    __version__ = "0.1.0a2"
+    __version__ = "0.1.0a3"
